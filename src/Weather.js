fa3f59c12678387205954448f87c127aef11006b
ajdaflajs <65505871+ajdaflajs@users.noreply.github.com>
2020-11-03 21:48:56 +0100

    axios and alert delete cleaner code

--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,8 @@
 import React from "react";
-import axios from "axios";
 import SearchEngine from "./SearchEngine";
 
 export default function Weather() {
-    function handleResponse(response){
-        alert (`Weather in London is ${Math.round(response.data.main.temp)}°C`)
-    }
-
-    let apiKey = "91dd9571694065e99a37dfcdc3f1bcb5"
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`
-
-    axios.get(apiUrl).then(handleResponse);
-    
+       
     return (
         <div>
           <h2>Hello React Weather App</h2>
