79441330637d0ca5d8df37a9a1c8eb42f7e303d4
ajdaflajs <65505871+ajdaflajs@users.noreply.github.com>
2020-11-03 20:28:24 +0100

    Math.round temperature and let units add on

--- src/Weather.js
+++ src/Weather.js
@@ -4,11 +4,12 @@ import axios from "axios";
 export default function Weather() {
     
     function handleResponse(response){
-        alert (`Weather in London is ${response.data.main.temp}`)
+        alert (`Weather in London is ${Math.round(response.data.main.temp)}`)
     }
 
+    let units = "metric"
     let apiKey = "91dd9571694065e99a37dfcdc3f1bcb5"
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&${units}`
 
     axios.get(apiUrl).then(handleResponse);
     return <div>Hello World 002</div>;
