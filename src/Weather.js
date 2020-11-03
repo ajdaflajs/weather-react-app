8b5277732fd08f1ebcbee8aebf3ac8f0eecc186b
ajdaflajs <65505871+ajdaflajs@users.noreply.github.com>
2020-11-03 20:49:08 +0100

    update on temperature alert

--- src/Weather.js
+++ src/Weather.js
@@ -4,12 +4,11 @@ import axios from "axios";
 export default function Weather() {
     
     function handleResponse(response){
-        alert (`Weather in London is ${Math.round(response.data.main.temp)}`)
+        alert (`Weather in London is ${Math.round(response.data.main.temp)}°C`)
     }
 
-    let units = "metric"
     let apiKey = "91dd9571694065e99a37dfcdc3f1bcb5"
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&${units}`
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`
 
     axios.get(apiUrl).then(handleResponse);
     return <div>Hello World 002</div>;
