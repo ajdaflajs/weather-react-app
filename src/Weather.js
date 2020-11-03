fa681c9372333bfa7ddf16ae2cb4f32a53bba511
ajdaflajs <65505871+ajdaflajs@users.noreply.github.com>
2020-11-03 21:58:43 +0100

    added GitHub link to react weather app

--- src/Weather.js
+++ src/Weather.js
@@ -4,10 +4,16 @@ import SearchEngine from "./SearchEngine";
 export default function Weather() {
        
     return (
-        <div>
-          <h2>Hello React Weather App</h2>
-          <SearchEngine />
+        <div className="Container">
+          <div className="SearchEngine">
+            <h2>Hello React Weather App</h2>
+            <SearchEngine />
+          </div>
+          <div>
+              <a href="https://github.com/ajdaflajs/weather-react-app">GitHub</a>
+          </div>
         </div>
+
     );
     
 }
