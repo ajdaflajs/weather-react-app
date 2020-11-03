c9e2ec55085dfdb570213cc9d0de01a5be6df71e
ajdaflajs <65505871+ajdaflajs@users.noreply.github.com>
2020-11-03 21:16:37 +0100

    new file SearcEngine for weather app with function SearchEngine AND axios with alert in Weather.js

---
+++ src/SearchEngine.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function SearchEngine() {
+  const [city, setCity] = useState(" ");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "91dd9571694065e99a37dfcdc3f1bcb5";
+    let units = "metric";
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiURL).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city..."
+        onChange={updateCity}
+      />
+      <button type="Submit">Search Now</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,9 @@
 import React from "react";
 import axios from "axios";
 
+import SearchEngine from "./SearchEngine";
+
 export default function Weather() {
-    
     function handleResponse(response){
         alert (`Weather in London is ${Math.round(response.data.main.temp)}°C`)
     }
@@ -11,6 +12,12 @@ export default function Weather() {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`
 
     axios.get(apiUrl).then(handleResponse);
-    return <div>Hello World 002</div>;
+    
+    return (
+        <div>
+          <h2>Hello World 002</h2>
+          <SearchEngine />
+        </div>
+    );
     
 }
