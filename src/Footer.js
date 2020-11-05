8db97a63a6592c1863884c3414d92d8c7d5d21e1
ajdaflajs <65505871+ajdaflajs@users.noreply.github.com>
2020-11-05 19:02:15 +0100

    import axios to footer for future coding

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,21 @@
 import React from "react";
+// import axios from "axios";
 
 export default function Footer() {
+
+    // axios.get(apiURL).then(showWeeklyForecast);
+
     return(
-        <div>
-            <p>Created by <span href="#">Ajda Flajs</span>
-            <br/>
-            <a href="https://github.com/ajdaflajs/weather-react-app">GitHub</a>
-            </p>
+        <div className="Footer">
+            <div className="WeeksForecast">
+                Weekly Weather Forecast
+            </div>
+            <div className="Author">
+              <p>Created by <span href="#">Ajda Flajs</span>
+              <br/>
+              <a href="https://github.com/ajdaflajs/weather-react-app">GitHub</a>
+              </p>
+            </div>
         </div>
     )
 }
