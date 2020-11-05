d7340440db92f012bb24702e6bb97de289110ede
ajdaflajs <65505871+ajdaflajs@users.noreply.github.com>
2020-11-05 20:10:36 +0100

    links styling author.css and.js

--- src/Author.js
+++ src/Author.js
@@ -1,12 +1,18 @@
 import React from "react";
+import "./author.css";
 
 export default function Author() {
     return (
       <div className="Author">
-        <p>Created by <span href="#">Ajda Flajs</span>
+        <div>Created by 
+            <a href="https://www.linkedin.com/in/ajdaflajs/" target="_blank">
+              <span className="Link"> Ajda Flajs</span>
+            </a>
         <br/>
-        <a href="https://github.com/ajdaflajs/weather-react-app">GitHub</a>
-        </p>
+        <a href="https://github.com/ajdaflajs/weather-react-app" target="_blank">
+            <span className="Link">GitHub</span>
+        </a>
+        </div>
       </div>
     );
 }
