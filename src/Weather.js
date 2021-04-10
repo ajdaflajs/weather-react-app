17ec6e8952ec0e713cae41912cc8eae9f0f06d33
ajdaflajs <65505871+ajdaflajs@users.noreply.github.com>
2021-04-10 17:15:19 +0200

    rearranging the main components for Weather

--- src/Weather.js
+++ src/Weather.js
@@ -7,8 +7,8 @@ export default function Weather() {
        
     return (
         <div className="Weather">
-            <SearchEngine />
             <MainForecast />
+            <SearchEngine />
             <WeeklyForecast />
         </div>
 
